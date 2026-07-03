4be81924cb39318b0d04d065b962a26cc8dec51c
uvais8958 <uvaisa740@gmail.com>
2026-07-03 11:42:19 +0530

    Delete and update functionality is completed

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -1,3 +1,5 @@
+
+
 //all global variable declaration
 let userInfo;
 let user;
@@ -9,6 +11,7 @@ let allBInput=bookingForm.querySelectorAll("input");
 let bookingTextArea=bookingForm.querySelector("textarea");
 let bookingCloseBtn=document.querySelector(".b-model-close-btn");
 let bListTBody=document.querySelector(".booking-list");
+let bRegBtn=document.querySelector(".b-registration-btn")
 //check user is login or not
 
 if(sessionStorage.getItem("user")==null){
@@ -33,7 +36,7 @@ const fetchData=(key)=>{
 
 //formate date function
 const formatDate=(data,isTime)=>{
-   console.log(isTime)
+   
    const date=new Date(data);
    let yy=date.getFullYear();
    let mm=date.getMonth()+1;
@@ -46,7 +49,6 @@ const formatDate=(data,isTime)=>{
   
 }
 allBookingData=fetchData(user+"_allBookingData");//
-console.log(allBookingData);
 
 
 
@@ -87,6 +89,69 @@ bookingForm.onsubmit=(e)=>{
    showBookingData();
 }
 
+
+//booking delete coding
+
+ const deleteBtnDataFunc=()=>{
+let allBdelBtn=bListTBody.querySelectorAll(".del-btn");
+allBdelBtn.forEach((btn,index)=>{
+   btn.onclick=()=>{
+   swal({
+      tittle:"Are you sure?",
+      text:"once deleted,you will not be able to recover this imaginary file!",
+      icon:"warning",
+      buttons:true,
+      dengerMode:true,
+   })
+   .then((willDeleted)=>{
+      if(willDeleted){
+         allBookingData.splice(index,1);
+         localStorage.setItem(user+"_allBookingData",JSON.stringify(allBookingData));
+         showBookingData();
+         swal("proof! your imaginary file has been save")({
+            icone:"Success",
+
+         });
+      }else{
+         swal("your imaginary file is safe!");
+      }
+   });
+}
+})
+ 
+
+ }
+
+
+//  updateBtn coding
+const updateDataFunction=()=>{
+   let allEditBtn=bListTBody.querySelectorAll(".edit-btn");
+      allEditBtn.forEach((btn,index)=>{
+      btn.onclick=()=>{
+         bRegBtn.click();
+         let allBBtn=bookingForm.querySelectorAll("button");
+         allBBtn[0].classList.add("d-none");
+         allBBtn[1].classList.remove("d-none");
+         let tr=btn.parentElement.parentElement;
+         let allTd=tr.querySelectorAll("td");
+         
+         //update data for user
+         allBInput[0].value=allTd[3].innerHTML;
+         allBInput[1].value=allTd[1].innerHTML;
+         allBInput[2].value=allTd[2].innerHTML;
+         allBInput[3].value=allTd[6].innerHTML;
+         allBInput[4].value=allTd[4].innerHTML;
+         allBInput[5].value=allTd[6]
+
+         
+
+      }
+      })   
+}
+
+
+
+
 //show booking data
 const showBookingData=()=>{
    
@@ -104,19 +169,25 @@ const showBookingData=()=>{
                             <td class="text-nowrap">${item.notice}</td>
                             <td class="text-nowrap">${formatDate(item.createdAt,true)}</td>
                             <td class="text-nowrap">
-                            <button class="btn p-1 px-2 btn-primary" >
+                            <button class="btn edit-btn p-1 px-2 btn-primary" >
                              <i class="fa fa-edit"></i>
                             </button>
-                            <button class="btn p-1 px-2 text-white btn-info" >
+                            <button class="btn checkin-btn p-1 px-2 text-white btn-info" >
                                 <i class="fa  fa-check"></i>
                             </button>
-                            <button class="btn p-1 px-2 btn-danger" >
+                            <button class="btn del-btn p-1 px-2 btn-danger" >
                                 <i class="fa fa-trash"></i>
                             </button>
                             </td>
                          </tr>`
                         
   })
+deleteBtnDataFunc();
+updateDataFunction();
+  
 }
-
 showBookingData()
+
+
+
+
